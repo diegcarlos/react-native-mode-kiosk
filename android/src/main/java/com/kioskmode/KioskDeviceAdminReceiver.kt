914a599c84c15ef38036286333c70d930710ce41
Diego Carlos <diegcarlos@gmail.com>
2024-11-04 14:30:38 -0400

    "Updated package name in KioskDeviceAdminReceiver.kt and bumped version in package.json from 0.0.3 to 0.0.5"

--- android/src/main/java/com/kioskmode/KioskDeviceAdminReceiver.kt
+++ android/src/main/java/com/kioskmode/KioskDeviceAdminReceiver.kt
@@ -1,4 +1,4 @@
-package com.reactnativekioskmode
+package com.kioskmode
 
 import android.app.admin.DeviceAdminReceiver
 import android.content.Context
@@ -7,9 +7,11 @@ import android.content.Intent
 class KioskDeviceAdminReceiver : DeviceAdminReceiver() {
     override fun onEnabled(context: Context, intent: Intent) {
         super.onEnabled(context, intent)
+        // Ativado como administrador do dispositivo
     }
 
     override fun onDisabled(context: Context, intent: Intent) {
         super.onDisabled(context, intent)
+        // Removido como administrador do dispositivo
     }
 }
